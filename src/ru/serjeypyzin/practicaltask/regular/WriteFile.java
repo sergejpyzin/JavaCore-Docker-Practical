c454e3b905b04e89460a2b69968fbd2cf4c94917
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-12-26 15:20:47 +0300

    changed WriteFile.java

--- src/ru/serjeypyzin/practicaltask/regular/WriteFile.java
+++ src/ru/serjeypyzin/practicaltask/regular/WriteFile.java
@@ -1,4 +1,21 @@
 package ru.serjeypyzin.practicaltask.regular;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WriteFile {
+
+    public void fileWriter(String filePath, String notes){
+
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath, true))) {
+            bufferedWriter.write(notes);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            System.out.println("Exception: " + e);
+        }
+
+
+    }
 }
