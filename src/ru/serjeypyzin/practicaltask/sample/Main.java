f07f27c1f4bcc8b52b5c46d52fb10f79d1e92952
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-12-26 15:21:17 +0300

    changed Main.java

--- src/ru/serjeypyzin/practicaltask/sample/Main.java
+++ src/ru/serjeypyzin/practicaltask/sample/Main.java
@@ -1,4 +1,9 @@
 package ru.serjeypyzin.practicaltask.sample;
 
+import ru.serjeypyzin.practicaltask.regular.UserInteraction;
+
 public class Main {
+    public static void main(String[] args) {
+        UserInteraction.runApp();
+    }
 }
